7804bf6184e879c2fece9f1240d0f240e146670a
Joseph Chretien-Golden <joechre@gmail.com>
2020-02-16 23:03:33 -0600

    adds msform container and classes for progress

---
+++ src/components/MultiStepForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <article>
+      <header>
+        <div class="progress">
+          <div class="progress-step"
+          :class="{'active':index === activeStep}"
+          v-for="(step, index) in formSteps"
+          :key="'step'+index">
+            {{ index + 1 }}
+          </div>
+        </div>
+      </header>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
